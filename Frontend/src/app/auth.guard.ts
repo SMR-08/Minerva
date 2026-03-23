273e7e155bd59f81e5b28bf9a3dc948d9f0335e0
rben-sq <rsanqui0208@g.educaand.es>
2026-03-23 18:40:43 +0100

    feat: landing y formularios con nuevo diseno

--- Frontend/src/app/auth.guard.ts
+++ Frontend/src/app/auth.guard.ts
@@ -1,15 +1,19 @@
-import { inject } from '@angular/core';
-import { Router, CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
-export const authGuard: CanActivateFn = () => {
-  const authService = inject(AuthService);
-  const router = inject(Router);
-  
-  if (authService.isLoggedIn()) {
-    return true;
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      return false;
+    }
   }
-  
-  router.navigate(['/login']);
-  return false;
-};
+}
